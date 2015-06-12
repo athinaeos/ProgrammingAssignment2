be41961221e9c07edde9ac94e41f8499d1d2dd73
Nikolaos Moropoulos <nikolaos.moropoulos@gmail.com>
2015-06-12 10:59:32 +0300

    created the two functions
    I simply replaced "mean" with "inverse" in the code provided in the
    assignment description.

--- cachematrix.R
+++ cachematrix.R
@@ -17,11 +17,31 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        t <-NULL
+        set <- function(y) {
+                x <<- y
+                t <<- NULL
+                }
+        get <- function() x
+        setinverse <- function(inverse) t <<- inverse
+        getinverse <- function() t
+        list(set = set, get = get, 
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        t <- x$getinverse()
+        if (!is.null(t)) {
+                message("getting cached data")
+                return (t)
+        }
+        data <- x$get()
+        t <- solve(data, ...)
+        x$setinverse(t)
+        t
 }
